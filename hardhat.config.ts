b33ca9654717e27fb2379a103d52a606106148d1
AntonD3 <antonv.dyadyuk@gmail.com>
2023-08-28 01:03:46 +0200

    Integrate CI, update license, cleanup repository

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -27,25 +27,8 @@ export default {
     },
   },
   defaultNetwork: 'zkSyncLocalhost',
-  etherscan: {
-    // Your API key for Etherscan
-    // Obtain one at https://etherscan.io/
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
   solidity: {
     version: '0.7.6',
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 800,
-      },
-      metadata: {
-        // do not include the metadata hash, since this is machine dependent
-        // and we want all generated code to be deterministic
-        // https://docs.soliditylang.org/en/v0.7.6/metadata.html
-        bytecodeHash: 'none',
-      },
-    },
   },
   zksolc: {
     version: '1.3.13',
